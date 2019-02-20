bd9db392f328423ef6d94ca7ef651569d4a7fad3
Momo Rao <monicaxrao@gmail.com>
2019-02-20 16:18:21 -0500

    feat: use TypeScript, and HOC higher order component method

---
+++ src/Message.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import messageHoc from './Hoc';
+
+// interface UserMessage {
+//     name: string;
+//     message: string;
+// }
+
+// const Message = (props: UserMessage): any => {
+//     return (
+//         <p>{props.name}, {props.message}</p>
+//     )
+// }
+
+const example = (props: any): any => <p>{props.name}, {props.message}</p>
+
+const Message = messageHoc(example);
+
+export default Message
